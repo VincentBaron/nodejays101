757568dc861ed1114f301c53db58337f1afa7551
VincentBaron <vbaron@student.42.fr>
2025-03-28 12:27:26 +0100

    chore: changed sequelize connection to simple connection string for fly.io postgres

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -2,17 +2,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const pg = require("pg");
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: "postgres",
-    dialectModule: pg,
-    logging: false, // Disable logging; set to true for debugging
-  }
-);
+// Use a connection string from the environment variable
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectModule: pg,
+  logging: false, // Disable logging; set to true for debugging
+});
 
 module.exports = sequelize;
