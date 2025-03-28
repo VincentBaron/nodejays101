954bded5b5c330fd3b95a6f97eb939e073886154
VincentBaron <vbaron@student.42.fr>
2025-03-28 14:21:49 +0100

    fix: back to connection string ....

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -2,23 +2,17 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const pg = require("pg");
 
-// Use individual parameters for the database connection
-const sequelize = new Sequelize(
-  process.env.POSTGRES_DATABASE, // Database name
-  process.env.POSTGRES_USER, // Username
-  process.env.POSTGRES_PASSWORD, // Password
-  {
-    host: process.env.POSTGRES_HOST, // Hostname
-    port: process.env.POSTGRES_PORT, // Port
-    dialect: "postgres",
-    dialectModule: pg,
-    logging: false, // Disable logging; set to true for debugging
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false, // Allow self-signed certificates
-      },
+// Use a connection string from the environment variable
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectModule: pg,
+  logging: false, // Disable logging; set to true for debugging
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Allow self-signed certificates
     },
-  }
-);
+  },
+});
 
 module.exports = sequelize;
